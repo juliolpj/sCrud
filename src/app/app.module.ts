cdd28f4956c23d5274a564c0bcf2a9b44375dee7
Julio Pérez <juliolpj@hotmail.com>
2019-03-31 18:18:08 -0300

    instalr ngx markdown, hacer copia del archivo readme.md en la carpeta assets, configurar en app.module, usar directiva markdown en el template del componente readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MarkdownModule } from 'ngx-markdown';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,6 +11,7 @@ import { HomeComponent } from './components/home/home.component';
 import { ReadmeComponent } from './components/readme/readme.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
+
 
 @NgModule({
   declarations: [
@@ -20,9 +24,13 @@ import { UsuariosComponent } from './components/usuarios/usuarios.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({ loader: HttpClient })
   ],
-  providers: [],
+  providers: [
+    HttpClient 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
