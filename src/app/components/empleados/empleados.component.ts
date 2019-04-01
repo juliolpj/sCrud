7f0077d1704e56b7574d92502012dce581b789aa
Julio Pérez <juliolpj@hotmail.com>
2019-04-01 10:36:36 -0300

    Crear arreglo empleados, llenar tabla a partir del arreglo con la directiva ngFor

--- src/app/components/empleados/empleados.component.ts
+++ src/app/components/empleados/empleados.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class EmpleadosComponent implements OnInit {
+  empleados = [
+    {
+      nombre: 'Julio Pérez',
+      cargo: 'Director',
+      email: 'juliolpj@hotmail.com'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.empleados);
+    
   }
 
+  agregar() {
+    console.log('Pulsó el botón agregar');
+    
+  }
+
+  eliminar(i: number) {
+    console.log('Eliminar', i);
+  }
+
+  seleccionar(i: number) {
+    console.log('Seleccionar', i);
+  }
 }
